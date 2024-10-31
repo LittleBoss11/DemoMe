5baab39133982793e7dbbf0902e928ffece09e35
Papik <Papik@LAPTOP-947VM9J3>
2024-10-31 12:59:56 +0300

    Добавьте файлы проекта.

---
+++ DemoWin/ClientInfo.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Demo
+{
+    partial class ClientInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CompanyNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DirectorTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.RatingTextBox = new System.Windows.Forms.TextBox();
+            this.DiscountTextTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CompanyNameTextBox
+            // 
+            this.CompanyNameTextBox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CompanyNameTextBox.Location = new System.Drawing.Point(236, 23);
+            this.CompanyNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CompanyNameTextBox.Name = "CompanyNameTextBox";
+            this.CompanyNameTextBox.Size = new System.Drawing.Size(132, 26);
+            this.CompanyNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(62, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название фирмы";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DirectorTextBox
+            // 
+            this.DirectorTextBox.Location = new System.Drawing.Point(237, 65);
+            this.DirectorTextBox.Name = "DirectorTextBox";
+            this.DirectorTextBox.Size = new System.Drawing.Size(131, 22);
+            this.DirectorTextBox.TabIndex = 2;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(236, 109);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(130, 22);
+            this.PhoneNumberTextBox.TabIndex = 3;
+            // 
+            // RatingTextBox
+            // 
+            this.RatingTextBox.Location = new System.Drawing.Point(235, 151);
+            this.RatingTextBox.Name = "RatingTextBox";
+            this.RatingTextBox.Size = new System.Drawing.Size(131, 22);
+            this.RatingTextBox.TabIndex = 4;
+            // 
+            // DiscountTextTextBox
+            // 
+            this.DiscountTextTextBox.Location = new System.Drawing.Point(235, 186);
+            this.DiscountTextTextBox.Name = "DiscountTextTextBox";
+            this.DiscountTextTextBox.Size = new System.Drawing.Size(130, 22);
+            this.DiscountTextTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Руководитель компании";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Номер телефона";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Рейтинг компании";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Скидка";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Вернуться";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(790, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 37);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(366, 418);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 49);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // ClientInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DiscountTextTextBox);
+            this.Controls.Add(this.RatingTextBox);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.DirectorTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CompanyNameTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ClientInfo";
+            this.Load += new System.EventHandler(this.ClientInfo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CompanyNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DirectorTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox RatingTextBox;
+        private System.Windows.Forms.TextBox DiscountTextTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ DemoWin/ClientInfo.cs
@@ -0,0 +1,44 @@
+﻿using DemoLib;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class ClientInfo : Form
+    {
+        public ClientInfo()
+        {
+            InitializeComponent();
+        }
+
+        public void SetClient(Client client)
+        {
+            CompanyNameTextBox.Text = client.Type + " " + client.Name;
+            PhoneNumberTextBox.Text = client.PhoneNumber;
+            DirectorTextBox.Text = client.Director;
+            RatingTextBox.Text = client.Rating.ToString();
+            DiscountTextTextBox.Text = client.Discount.ToString();
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ClientInfo_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
